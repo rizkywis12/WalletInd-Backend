800b506c83939418db9300db63149f8d25fd9e47
windynathalie <windy.nathalie@gmail.com>
2022-11-24 11:39:39 +0800

    Forgot Password

--- src/main/java/net/mujoriwi/walletind/model/dto/request/ForgotPasswordDto.java
+++ src/main/java/net/mujoriwi/walletind/model/dto/request/ForgotPasswordDto.java
@@ -11,9 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @AllArgsConstructor
-public class RegisterDto {
-
-    @NotBlank(message = "Email is required")
+public class ForgotPasswordDto {
     @Pattern(regexp = "^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$", message = "Please input correct email format! Ex: example@domain.com")
     private String email;
 
@@ -21,7 +19,7 @@ public class RegisterDto {
     @Size(min = 8, max = 12, message = "Password must between 8 - 12 characters")
     private String password;
 
-    @NotBlank(message = "User name is required")
-    @Pattern(regexp = "^[a-zA-Z0-9._-]{3,}$", message = "input minimal 3 characters and only accepts a-z, A-Z, 0-9, ., - and _")
-    private String userName;
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 12, message = "Password must between 8 - 12 characters")
+    private String confirmPassword;
 }
